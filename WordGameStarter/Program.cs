32c6950543555758d954cedd02394805c7d63685
Bryce Freshwater <bfreshwater@gmail.com>
2024-05-31 18:07:44 -0400

    Add project files.

---
+++ WordGameStarter/Program.cs
@@ -0,0 +1,138 @@
+﻿using PriorityQueue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WordGameStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<WordGame> wordGames = new List<WordGame>();
+            char playAgainInput = '0';
+            bool playAgain = true;
+            int wordLength = 0;
+            int numWords;
+            while (playAgain == true)
+            {
+                Console.WriteLine("How many games would you like to play?");
+                numWords = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("How many letters?");
+                wordLength = Convert.ToInt16(Console.ReadLine());
+                for (int i = 0; i < numWords; i++)
+                {
+                    WordGame game = new WordGame(wordLength, numWords);
+                    game.GenerateNextWord();
+                    game.setPriority();
+                    wordGames.Add(game);
+                }
+                WordGame temp;
+                for (int j = 0; j < wordGames.Count - 2; j++)
+                {
+                    for (int i = 0; i <= wordGames.Count -2; i++)
+                    {
+                        if (wordGames[i].GetPriority() > wordGames[i+1].GetPriority())
+                        {
+                            temp = wordGames[i + 1];
+                            wordGames[i + 1] = wordGames[i];
+                            wordGames[i] = temp;
+                        }
+                    }
+                }
+                PriorityQueue<WordGame> queue = new PriorityQueue<WordGame>();
+                foreach (WordGame game in wordGames)
+                {
+                    queue.Add(game.GetPriority(), game);
+                }
+
+                while (queue.Count > 0)
+                {
+                    WordGame currGame = queue.Remove();
+                    Console.WriteLine("This words difficulty is " + currGame.GetPriority());
+                    char menuInput = '0';
+
+                    while (menuInput != 'A' && menuInput != 'a' && menuInput != 'Q')
+                    {
+                        Console.WriteLine("G)uess");
+                        Console.WriteLine("H)istory");
+                        Console.WriteLine("R)etrieve");
+                        Console.WriteLine("A)bandon");
+                        menuInput = Console.ReadKey().KeyChar;
+                        Console.WriteLine();
+                        switch (menuInput)
+                        {
+                            //prompts user to guess the word and displays the number of letter matches
+                            case 'G':
+                            case 'g':
+                                string userGuess;
+                                Console.WriteLine("Guess the " + wordLength + " letter word");
+                                userGuess = Console.ReadLine();
+                                while (userGuess.Count<char>() != wordLength || currGame.wordExistsInList(userGuess) == false)
+                                {
+                                    Console.WriteLine("Enter a valid " + wordLength + " letter word");
+                                    userGuess = Console.ReadLine();
+                                }
+
+                                if (currGame.IsMatch(userGuess) == true)
+                                {
+                                    Console.WriteLine("You guessed the word! It took " + currGame.GuessCount + " guess");
+                                    menuInput = 'Q';
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Nope! " + currGame.GetCount(userGuess) + " letters correct");
+                                    currGame.addGuessList(userGuess, currGame.GetCount(userGuess));
+                                }
+                                break;
+
+                            //displays all the guesses and associated word counts in order guessed
+                            case 'H':
+                            case 'h':
+                                currGame.displayGuessList();
+                                break;
+
+                            //prompts the user to enter a new word; returns the number of matches if the word
+                            //has been guessed previously. If not, it informs the user that the word has not been selected
+                            //before.Do not recalculate the number of matches; look them up.
+                            case 'R':
+                            case 'r':
+                                Console.WriteLine("Enter a word to retrieve");
+                                currGame.Retrieve(Console.ReadLine());
+                                break;                                
+                        }
+
+                        if (menuInput == 'a' || menuInput == 'A')
+                        {
+                            Console.WriteLine("The word was " + currGame.Target);
+                            break;
+                        }    
+                    }
+                }
+                Console.WriteLine("Would you like to play again?");
+                Console.WriteLine("Y)es");
+                Console.WriteLine("N)o");
+                playAgainInput = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                switch (playAgainInput)
+                {
+                    case 'Y':
+                    case 'y':
+                        playAgain = true;
+                        break;
+
+                    case 'N':
+                    case 'n':
+                        playAgain = false;
+                        break;
+                }
+            }
+        }
+    }              
+}
---
+++ WordGameStarter/WordGame.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PriorityQueue;
+
+namespace WordGameStarter
+{
+    class WordGame
+    {
+        public const int BAD_LENGTH = -2;
+        public const int NO_SUCH_WORD = -1;
+        private const string DICTIONARY_LOCATION = "C:\\wordlist.txt";
+        private int wordLength;
+        private int numWords;
+        static private List<string> wordList = null;
+        List<string> filteredWordList = new List<string>();
+        IDictionary<string, int> wordLengthDictionary = new Dictionary<string, int>();
+        List<string> guessList = new List<string>();
+        List<int> guessCountList = new List<int>();
+        private int priority = 0;
+        public string Target { get; private set; }
+        public int GuessCount { get; private set; }
+
+        public WordGame(int length)
+        {
+            wordLength = length;
+            if (wordList == null)
+                LoadList();
+            if (wordLengthDictionary.Count == 0)
+                CreateDictionary();
+        }
+
+        // You'll want this for an 'A'
+        public WordGame(int length, int numWords) : this(length)
+        {
+            this.numWords = numWords;
+            if (wordList == null)
+                LoadList();
+            if (wordLengthDictionary.Count == 0)
+                CreateDictionary();
+        }
+
+        /*
+         * Get a random word from the list; make certain it fits the criteria,
+         * e.g. correct length, no repeated letters (for a 'C' only).
+         */
+        public void GenerateNextWord()
+        {
+            //foreach (var word in wordLengthDictionary)
+            foreach (KeyValuePair<string, int> word in wordLengthDictionary)
+            {
+                if (word.Value == wordLength)
+                {
+                    filteredWordList.Add(word.Key);
+                }
+            }
+            Random r = new Random();
+            int index = r.Next(filteredWordList.Count);
+            Target = filteredWordList[index];
+        }
+
+        public void setPriority()
+        {
+            int vowelCount = 0;
+            int rareLetterCount = 0;
+            int sameLetterCounter = 0;
+            Dictionary<char, int> targetLetterDictionary = new Dictionary<char, int>();
+            if (Target[Target.Length - 1] != 's')
+            {
+                priority++;
+            }
+            if (Target[0] == 'a' || Target[0] == 'e' || Target[0] == 'i' || Target[0] == 'o' || Target[0] == 'u')
+            {
+                priority++;
+            }
+            for (int i = 0; i< Target.Length; i++)
+            {
+                if (Target[i] == 'a' || Target[i] == 'e' || Target[i] == 'i' || Target[i] == 'o' || Target[i] == 'u')
+                {
+                    vowelCount++;
+                }
+            }
+            if ( vowelCount >= 2 ) { priority++; }
+            for (int i = 0; i < Target.Length; i++)
+            {
+                if (Target[i] == 'q' || Target[i] == 'v' || Target[i] == 'w' || Target[i] == 'x' || Target[i] == 'y' || Target[i] == 'z')
+                {
+                    rareLetterCount++;
+                }
+            }
+            if ( rareLetterCount >= 1 ) { priority++; }
+            for(int i = 0; i < Target.Length;i++)
+            {
+                if (targetLetterDictionary.ContainsKey(Target[i]))
+                {
+                    targetLetterDictionary[Target[i]]++;
+                }
+                else { targetLetterDictionary.Add(Target[i], 1); }   
+            }
+            foreach (var item in targetLetterDictionary)
+            {
+                if (item.Value >= 3)
+                {
+                    priority = priority + 4; break;
+                }
+            }
+            foreach (var item in targetLetterDictionary)
+            {
+                if (item.Value == 2)
+                {
+                    priority = priority + 2;
+                }
+            }
+        }
+
+        public int GetPriority() { return priority; }
+
+        private void LoadList()
+        {
+            StreamReader input = new StreamReader(DICTIONARY_LOCATION);
+            wordList = new List<string>();
+            while (!input.EndOfStream)
+            {
+                wordList.Add(input.ReadLine());
+            }
+        }
+
+        private void CreateDictionary()
+        {
+            foreach (string word in wordList)
+            {
+                if (wordLengthDictionary.ContainsKey(word))
+                {
+                    break;
+                }
+                else { wordLengthDictionary.Add(word, word.Length); }
+            }
+        }
+
+        /*
+         * Is this the exact word?
+         */
+        public bool IsMatch(string guess)
+        {
+            if ( guess == Target)
+            {
+                GuessCount++;
+                return true;
+            }
+            else
+            {
+                GuessCount++;
+                return false;
+            }    
+        }
+        
+        /*
+         * This code retuns the correct count for non-duplicated letters,
+         * but it does not check that the word is valid for this game.
+         * this includes length and the existence of the word guessed.
+         * Add that code.  Check out wordExists below.
+         */
+        public int GetCount(string guess)
+        {
+            int count = 0;
+            // For the 'C' code, we don't want to count duplicates in our
+            // target, so the GenerateNextWord should keep duplicates out.
+            // This will not be a problem for 'B' code.
+            for (int i=0; i < guess.Length; i++)
+            {
+                if (guess.Substring(i + 1).Contains(guess[i]))
+                {
+                    guess = guess.Remove(i, 1);
+                    i--;
+                }
+
+            }
+            for (int i=0; i < guess.Length; i++)
+            {
+                if (Target.Contains(guess[i]))
+                        count++;
+            }
+
+            return count;
+        }
+
+        /*
+         * The word list is a little broken in that some plurals are missing,
+         * as are the third person single entries for some verbs.  For this reason
+         * I have provided this method to check whether the word exists.
+         * Essentially, if a word seems to be plural, it checks to see whether the
+         * singular form exists.
+         * 
+         * This algorithm allows non-existent words (e.g. filess, or larges)
+         * but it is good enough for this program, unless you feel inspired.  It is
+         * possible that there are words that are not caught if the construction is
+         * not done by merely adding an 's', e.g. relaxes.
+         * 
+         * You should not need to modify this code.
+         */
+        private bool wordExists(string word)
+        {
+            word = word.ToLower();
+            bool found = false;
+            if (wordExistsInList(word))
+                found = true;
+            else if (word[word.Length - 1] == 's' && wordExistsInList(word.Substring(0, wordLength - 1)))
+                found = true;
+            return found;
+        }
+
+        public bool wordExistsInList(string word)
+        {
+            return wordLengthDictionary.ContainsKey(word);
+        }
+
+        public void addGuessList(string guess, int count)
+        {
+            guessList.Add(guess);
+            guessCountList.Add(count);
+
+        }
+        public void displayGuessList()
+        {
+            for(int i = 0; i<guessList.Count; i++)
+            {
+                Console.WriteLine(guessList[i] + " - " + guessCountList[i] + " correct letters");
+            }
+        }
+
+        public void Retrieve(string word)
+        {
+            for(int i = 0; i<guessList.Count; i++)
+            {
+                if (guessList[i] == word)
+                {
+                    Console.WriteLine(word + " - " + guessCountList[i]);
+                    return;
+                }
+            }
+            Console.WriteLine("Word was not found");
+        }
+    }
+}
